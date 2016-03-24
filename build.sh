b66f6f0171434ec98646b13c304047b3bba0fe26
Kevin Wang <wangkevin448@gmail.com>
2016-03-24 00:19:40 -0700

    build failing

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "This script uses functionality which requires root privileges"
+  exit 1
+fi
+
+# installs acbuild
+if ! [ -e "$PWD/build_modules/acbuild" ]; then
+  wget https://github.com/appc/acbuild/releases/download/v0.2.2/acbuild.tar.gz && tar -xvzf acbuild.tar.gz -C "$PWD/build_modules" && rm acbuild.tar.gz
+fi
+
+# installs rkt
+if ! [ -e "$PWD/build_modules/rkt" ]; then
+  wget https://github.com/coreos/rkt/releases/download/v1.2.1/rkt-v1.2.1.tar.gz -O rkt.tar.gz && mkdir "$PWD/build_modules/rkt" && tar -xvzf rkt.tar.gz -C "$PWD/build_modules/rkt" --strip-components 1 && rm rkt.tar.gz
+fi
+
+# Start the build with an empty ACI
+./build_modules/acbuild --debug begin
+
+# In the event of the script exiting, end the build
+acbuildEnd() {
+  export EXIT=$?
+  ./build_modules/acbuild --debug end && exit $EXIT
+}
+trap acbuildEnd EXIT
+
+# Name the ACI
+./build_modules/acbuild --debug set-name xorkevin/bullet-battery
+
+# Based on alpine
+./build_modules/acbuild --debug dep add quay.io/coreos/alpine-sh
+
+# Install nginx
+./build_modules/acbuild --debug run apk update
+./build_modules/acbuild --debug run apk add nginx
+
+# Add a port for http traffic over port 80
+./build_modules/acbuild --debug port add http tcp 80
+
+# Add a mount point for files to serve
+./build_modules/acbuild --debug mount add html /usr/share/nginx/html
+
+# Run nginx in the foreground
+./build_modules/acbuild --debug set-exec -- /usr/sbin/nginx -g "daemon off;"
+
+# Version info
+./build_modules/acbuild label add version 0.1.0
+./build_modules/acbuild label add arch amd64
+./build_modules/acbuild label add os linux
+./build_modules/acbuild annotation add authors "xorkevin <wangkevin448@gmail.com>"
+
+# Save the ACI
+./build_modules/acbuild --debug write --overwrite bullet-battery.aci
---
+++ docs/install.md
@@ -0,0 +1,4 @@
+# Installation Guide
+
+Install Go
+Run build.sh
